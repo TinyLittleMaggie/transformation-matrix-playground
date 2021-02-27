adcdf9b38148de1fb7330d2ee6dc03be2d88bd46
Maggie Gong <meijic@outlook.com>
2021-02-27 17:24:40 +0800

    Add script for pagination

--- assets/js/levels.js
+++ assets/js/levels.js
@@ -1,5 +1,8 @@
 /* ---------------------------- Data & Elements ---------------------------- */
 
+// Keep track of the current level
+var currentLevel = 0;
+
 // Store content of different pages
 var levels = [
   {
@@ -29,11 +32,13 @@ var levels = [
 ];
 
 // Select elements on the page
-var currentLevel = 0;
 var navTop = document.getElementById('nav-top');
 var navButtons = document.querySelectorAll('.nav-btn');
 var title = document.getElementById('page-title');
 var content = document.getElementById('page-content');
+var dotsContainer = document.getElementById('dots');
+var previous = document.getElementById('previous');
+var next = document.getElementById('next');
 
 /* ------------------------------- Functions ------------------------------- */
 
@@ -44,16 +49,63 @@ function loadLevel(index) {
   content.innerHTML = levels[index].content;
   // Update current level
   currentLevel = index;
+  // Create pagination dots
+  createDots();
+  // Update previous & next buttons
+  updatePrevAndNext();
+  // Update icons
+  updateIcons(index);
 }
 
 // Set active icon as pink and the rest as grey
-function updateIcons(activeIcon) {
+function updateIcons(index) {
   // Remove "active" from all nav buttons
   navButtons.forEach(function(button) {
     button.classList.remove("active");
   });
-  // Add "active" to active icon
-  activeIcon.classList.add("active");
+  // Add "active" to icon based on index
+  if (index <= 1) {
+    navButtons[0].classList.add("active");
+  } else {
+    navButtons[index - 1].classList.add("active");
+  }
+}
+
+// Pagination: create as many dots as there are levels
+function createDots() {
+  // Clear the content of the dots container
+  dotsContainer.innerHTML = '';
+  // Create a new dot
+  var dot = document.createElement('li');
+  dot.classList.add('dot');
+  // Insert dots based on number of levels
+  levels.forEach(function() {
+    dotsContainer.appendChild(dot.cloneNode(true));
+  });
+  // Update colors for dots
+  updateDots();
+}
+
+// Visually indicate the current page
+function updateDots() {
+  var dots = document.querySelectorAll('.dot');
+  // Remove "active" from all dots
+  dots.forEach(function(dot) {
+    dot.classList.remove('active');
+  });
+  // Add "active" to the dot that indicates current page
+  dots[currentLevel].classList.add('active');
+}
+
+// Enable / disable the previous & next buttons
+function updatePrevAndNext() {
+  previous.classList.remove('disabled');
+  next.classList.remove('disabled');
+  if (currentLevel === 0) {
+    previous.classList.add('disabled');
+  } else if (currentLevel === levels.length - 1) {
+    next.classList.add('disabled');
+  }
 }
 
 /* ---------------------------- Event listeners ---------------------------- */
@@ -64,7 +116,20 @@ navTop.addEventListener('click', function(e) {
   var btnClicked = e.target.classList.contains("nav-btn");
   if (btnClicked) {
     loadLevel(Number(e.target.dataset.level));
-    updateIcons(e.target);
+  }
+});
+
+// Load the previous page
+previous.addEventListener('click', function() {
+  if (currentLevel !== 0) {
+    loadLevel(currentLevel - 1);
+  }
+});
+
+// Load the next page
+next.addEventListener('click', function() {
+  if (currentLevel !== levels.length - 1) {
+    loadLevel(currentLevel + 1);
   }
 });
 
