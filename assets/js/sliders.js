65a50a6cd3242424056fd433023d0cea7aae910c
Maggie Gong <meijic@outlook.com>
2021-03-07 20:36:33 +0800

    Add a single range slider to the "Translate" page

--- assets/js/sliders.js
+++ assets/js/sliders.js
@@ -1,50 +1,54 @@
-/* ---------------------- Select elements on the page ---------------------- */
+function initializeSliders() {
 
-var sliderContainer = document.querySelector('.range-slider-container');
-var sliderInput = document.querySelector('.range-slider');
-var minValue = sliderContainer.querySelector('.min-value');
-var maxValue = sliderContainer.querySelector('.max-value');
-var valueLabel = sliderContainer.querySelector('.value-label');
+  /* ---------------------- Select elements on the page ---------------------- */
 
-/* ------------------------------- Functions ------------------------------- */
+  var sliderContainer = document.querySelector('.range-slider-container');
+  var sliderInput = document.querySelector('.range-slider');
+  var minValue = sliderContainer.querySelector('.min-value');
+  var maxValue = sliderContainer.querySelector('.max-value');
+  var valueLabel = sliderContainer.querySelector('.value-label');
 
-// Calculate the horizontal position of the thumb (in percentage number)
-function getPercentage() {
-  var total = sliderInput.max - sliderInput.min;
-  return ((sliderInput.value - sliderInput.min)/total) * 100;
-}
+  /* ------------------------------- Functions ------------------------------- */
 
-// Display min & max values based on <input> attributes
-function setRangeLabels() {
-  minValue.innerText = sliderInput.min;
-  maxValue.innerText = sliderInput.max;
-}
+  // Calculate the horizontal position of the thumb (in percentage number)
+  function getPercentage() {
+    var total = sliderInput.max - sliderInput.min;
+    return ((sliderInput.value - sliderInput.min)/total) * 100;
+  }
 
-// Display the slider value right above the thumb
-function setValueLabel() {
-  // Display the value
-  valueLabel.innerText = sliderInput.value;
-  // Adjust the position
-  valueLabel.style.left = "calc(" + getPercentage() + "% - 17px)";
-}
+  // Display min & max values based on <input> attributes
+  function setRangeLabels() {
+    minValue.innerText = sliderInput.min;
+    maxValue.innerText = sliderInput.max;
+  }
 
-// Color the slider track to reflect current value
-function setTrackColors() {
-  var p = getPercentage();
-  var css = "linear-gradient(90deg, rgba(246, 84, 133, 1) " + p + "%, rgba(246, 84, 133, 0.2) " + p + "%)"
-  sliderInput.style.background = css;
-}
+  // Display the slider value right above the thumb
+  function setValueLabel() {
+    // Display the value
+    valueLabel.innerText = sliderInput.value;
+    // Adjust the position
+    valueLabel.style.left = "calc(" + getPercentage() + "% - 17px)";
+  }
 
-/* ---------------------------- Event listeners ---------------------------- */
+  // Color the slider track to reflect current value
+  function setTrackColors() {
+    var p = getPercentage();
+    var css = "linear-gradient(90deg, rgba(246, 84, 133, 1) " + p + "%, rgba(246, 84, 133, 0.2) " + p + "%)"
+    sliderInput.style.background = css;
+  }
 
-sliderInput.addEventListener("input", function() {
+  /* ---------------------------- Event listeners ---------------------------- */
+
+  sliderInput.addEventListener("input", function() {
+    setValueLabel();
+    setTrackColors();
+  });
+
+  /* ---------------------------- Initialization ----------------------------- */
+
+  setRangeLabels();
   setValueLabel();
   setTrackColors();
-});
 
-/* ---------------------------- Initialization ----------------------------- */
-
-setRangeLabels();
-setValueLabel();
-setTrackColors();
+}
 
